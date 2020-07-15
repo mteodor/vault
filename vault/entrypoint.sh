8c68db799b5bc4416702af4c96ca3c92e3f138df
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-07-15 14:36:37 +0200

    add unseal on startup
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- vault/entrypoint.sh
+++ vault/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo "mirko"
-echo $VAULT_UNSEAL_KEY_1
-echo ${VAULT_UNSEAL_KEY_2}
-echo ${VAULT_UNSEAL_KEY_3}
+
+
+vault server -config=/vault/config/vault-config.json&
+sleep 2
+
+echo "unseal vault"
+vault operator unseal ${VAULT_UNSEAL_KEY_1}
+vault operator unseal ${VAULT_UNSEAL_KEY_2}
+vault operator unseal ${VAULT_UNSEAL_KEY_3}
+
+# this is stupid way not to exit from the shell script
+# but I need to start the server and then to execute unseal
+# sequence and after that if shell script exits container exits
+while :
+do
+	sleep 10000
+done
+
